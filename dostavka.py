3d71266852a89ad9e2782c74a828c37d3d00c449
NATACHAOS <164019299+NATACHAOS@users.noreply.github.com>
2024-04-19 11:56:53 +0300

    Add files via upload

--- dostavka.py
+++ dostavka.py
@@ -1,14 +1,11 @@
 print("Расчёт стоимости доставки")
 price = 50
 price_for_10 = 200
-weight = int(input("Напишите вес посылки: "))
+weight = int(input("Напишите вес посылки в кг и нажмите Enter: "))
 if weight <= 2:
     print('Стоимость составляет:', price, 'рублей')
-if weight in range(3, 11):
-    price_dop = 0
-    for i in range(len(weight)):
-        price_dop += 20
-    print('Стоимость составляет:', price + (i), 'рублей')
-    print(price_dop)
-if weight > 11:
+if 2 < weight <= 10:
+    price = 50 + 20 * (weight - 2)
+    print('Стоимость составляет:', round(price, 2), 'рублей')
+if weight > 10:
     print('Стоимость составляет:', price_for_10, 'рублей')
